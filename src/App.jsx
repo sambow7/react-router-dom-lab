cc752c8ce6eb27755eb397535aee3cfeefe5912f
Sam Bowen <samhbowen@gmail.com>
2025-03-19 13:28:00 -0700

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ const initialState = [
   { id: 1, subject: 'Meeting Reminder', sender: 'John Doe', content: 'Don\'t forget about our meeting tomorrow at 10 AM' },
   { id: 2, subject: 'New Project Proposal', sender: 'Jane Smith', content: 'We need your expertise to review our proposal' },
   { id: 3, subject: 'Payment Reminder', sender: 'Mike Johnson', content: 'is this Working?' },
-
+];
 const App = () => {
   const [mailbox, setMailbox] = useState(initialState);
+
   return (
     <>
     <NavBar />
     <h1>Mailbox</h1>
     <MailboxList mailbox={mailbox} />
     </>
-  )
+  );
+  
 };
 
 export default App;
--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -1,3 +1,20 @@
+
+// src/components/MailboxDetails/MailboxDetails.jsx
+
+// const { mailboxId } = useParams();
+// const selectedBox = props.mailboxes.find((mailbox) => (
+//    mailbox._id === Number(mailboxId)
+// ));
+
+// src/components/MailboxDetails/MailboxDetails.jsx
+
+// const { mailboxId } = useParams();
+// const selectedLetters = props.letters.filter((letter) => (
+//   letter.mailboxId === Number(mailboxId)
+// ));
+
+
+
 const MailboxDetails = () => {
   return (
     <>
@@ -8,3 +25,5 @@ const MailboxDetails = () => {
     </>
   )
 }
+
+export default MailboxDetails;
